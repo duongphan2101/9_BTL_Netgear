53dc048ac18cba488da5fb8256a2e414094b8133
duong <duongphan1912@gmail.com>
2024-02-17 18:20:12 +0700

    first

---
+++ JS/sour.js
@@ -0,0 +1,36 @@
+var textArray = [
+    "sign up for speacial offers",
+    "find the right orbi for you",
+    "limited time offers ",
+    "upgrade to orbi wifi 7"
+  ];
+  var linkArray = [
+    "#",
+    "youtube.com",
+    "https://fb.com",
+    "https://tiktok.com"
+  ];
+
+  var currenIndex = 0;
+
+  function changeText() {
+    var changingText = document.getElementById("changingText");
+    changingText.innerHTML = "<a href='" + linkArray[currentIndex] + "' target='_blank'>" + textArray[currentIndex] + "</a>";
+    currentIndex = (currentIndex + 1) % textArray.length;
+  }
+  setInterval(changeText, 5000);
+
+
+
+  var listItem = document.getElementById('li_list');
+  var menu = document.getElementById('notice');
+
+  // Bắt sự kiện mouseover để hiển thị menu
+  listItem.addEventListener('mouseover', function () {
+    menu.style.display = 'block';
+  });
+
+  // Bắt sự kiện mouseout để ẩn menu
+  listItem.addEventListener('mouseout', function () {
+    menu.style.display = 'none';
+  });
