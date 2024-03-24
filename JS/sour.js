69e447bf0e2aae4e4d46f5cd06740907c3043b1a
duong <duongphan1912@gmail.com>
2024-03-24 17:02:11 +0700

    new page Sign In

--- JS/sour.js
+++ JS/sour.js
@@ -1,25 +1,43 @@
 var textArray = [
-    "sign up for speacial offers",
-    "find the right orbi for you",
-    "limited time offers ",
-    "upgrade to orbi wifi 7"
-  ];
-  var linkArray = [
-    "#",
-    "youtube.com",
-    "https://fb.com",
-    "https://tiktok.com"
-  ];
+  "find the right orbi for you >>",
+  "limited time offers >>",
+  "upgrade to orbi wifi 7 >>",
+  "sign up for special offers >>"
+];
 
-  var currenIndex = 0;
+var linkArray = [
+  "#",
+  "https://www.youtube.com",
+  "https://www.fb.com",
+  "https://www.tiktok.com"
+];
 
-  function changeText() {
-    var changingText = document.getElementById("changingText");
-    changingText.innerHTML = "<a href='" + linkArray[currentIndex] + "' target='_blank'>" + textArray[currentIndex] + "</a>";
-    currentIndex = (currentIndex + 1) % textArray.length;
-  }
-  setInterval(changeText, 5000);
+var currentIndex = 0;
 
+function changeText() {
+  var changingText = document.getElementById("changingText");
+  changingText.innerHTML = "<a href='" + linkArray[currentIndex] + "' target='_blank'>" + textArray[currentIndex] + "</a>";
+  currentIndex = (currentIndex + 1) % textArray.length;
+}
+
+setInterval(changeText, 5000);
+var next = document.getElementById('header-next');
+var prev = document.getElementById('header-prev');
+
+function updateText() {
+  var changingText = document.getElementById("changingText");
+  changingText.innerHTML = "<a href='" + linkArray[currentIndex] + "' target='_blank'>" + textArray[currentIndex] + "</a>";
+}
+
+next.addEventListener('click', function(){
+  currentIndex = (currentIndex + 1) % textArray.length;
+  updateText();
+});
+
+prev.addEventListener('click', function(){
+  currentIndex = (currentIndex - 1 + textArray.length) % textArray.length;
+  updateText();
+})
 
 
   var listItem = document.getElementById('li_list');
@@ -149,3 +167,7 @@ lg.addEventListener('mouseover', function(){
 lg.addEventListener('mouseout', function(){
   ul_lg.style.display = 'none';
 });
+
+function chuyenTrang(){
+  window.location.href = "signin.html";
+};
